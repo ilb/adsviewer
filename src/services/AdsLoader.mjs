ca549d4ccd6f3de45137b8d7b95e925dd9daac38
Mitrich <dima2prog@gmail.com>
2021-03-25 23:53:55 +0400

    AdsLoader черновой вариант

---
+++ src/services/AdsLoader.mjs
@@ -0,0 +1,34 @@
+export default class AdsLoader {
+  constructor({ adsProvider, adsAdapter, lastDateRepository, adsRepository, nameSource }) {
+    this.adsAdapter = adsAdapter;
+    this.adsProvider = adsProvider;
+    this.lastDateRepository = lastDateRepository;
+    this.nameSource = nameSource;
+    this.adsRepository = adsRepository;
+  }
+
+  async getLastDate() {
+    const nameSource = this.nameSource;
+    const date = this.lastDateRepository.getDate(nameSource);
+    return date.lastloaddate;
+  }
+
+  async setLastDate(data) {
+    const dataLastElem = data.pop();
+    // const lastDate = dataLastElem Тут будет поле из которого будем вытаскивать дату либо id
+    const nameSource = this.nameSource;
+    return this.lastDateRepository.setDate(nameSource, lastDate);
+  }
+
+  async loadData({ dateFrom, dateTo }) {
+    let setFromDate = dateFrom;
+    const lastDate = (await this.getLastDate()) || new Date();
+    if (!dateFrom) {
+      setFromDate = lastDate;
+    }
+    const data = await this.adsProvider.getAdsByDate({ setFromDate, dateTo });
+
+    this.setLastDate(data);
+    this.adsRepository.save(data);
+  }
+}
