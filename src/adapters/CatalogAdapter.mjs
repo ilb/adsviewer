5a18ef909515d38c0f1f8ffabf2e6c848320d066
tyort <neotyo1986@gmail.com>
2022-09-02 14:04:43 +0300

    Рефакторинг CatalogAdapter. Непотоковый адаптер

--- src/adapters/CatalogAdapter.mjs
+++ src/adapters/CatalogAdapter.mjs
@@ -1,4 +1,7 @@
 import xml2js from 'xml2js';
+import { promises as fs} from 'fs';
+import { promisify } from 'util';
+const readFileAsync = promisify(fs.readFile);
 
 export default class CatalogAdapter {
   constructor() {}
@@ -7,11 +10,9 @@ export default class CatalogAdapter {
    * @param {XML} data
    * @returns Object
    */
-  async convert(data) {
-    // var parser = new xml2js.Parser();
+  async convert() {
     const parser = new xml2js.Parser();
-    const result = await parser.parseStringPromise(data);
-
-    return result;
+    const fileData = await fs.readFile(process.env.XML_FILE_WRITER);
+    return await parser.parseStringPromise(fileData);
   }
 }
--- src/providers/CatalogProvider.mjs
+++ src/providers/CatalogProvider.mjs
@@ -1,13 +1,11 @@
-import { XMLParser } from 'fast-xml-parser';
+// import xml2js from 'xml2js';
 import got from 'got';
 import { createWriteStream, createReadStream } from 'fs';
 import stream from 'stream';
-import { Transform } from 'stream';
+// import { Transform } from 'stream';
 import { promisify } from 'util';
 const pipeline = promisify(stream.pipeline);
 
-const CATALOG_URL = 'https://autoload.avito.ru/format/Autocatalog.xml';
-const FILE_WRITER = 'test/data/catalog.xml';
 const TRANSFORMED_FILE_WRITER = 'test/data/catalog.json';
 import CatalogsAdapter from '../adapters/CatalogAdapter.mjs';
 export default class CatalogProvider {
@@ -18,8 +16,8 @@ export default class CatalogProvider {
   }
 
   async getCatalogXML() {
-    const downloadStream = got.stream(CATALOG_URL);
-    const fileWriterStream = createWriteStream(FILE_WRITER);
+    const downloadStream = got.stream(this.url);
+    const fileWriterStream = createWriteStream(process.env.XML_FILE_WRITER);
 
     downloadStream
       .on('downloadProgress', ({ transferred }) => {
@@ -31,43 +29,31 @@ export default class CatalogProvider {
 
     try {
       await pipeline(downloadStream, fileWriterStream);
-      console.log(`File downloaded to ${FILE_WRITER}`);
+      console.log(`File downloaded to ${process.env.XML_FILE_WRITER}`);
     } catch (error) {
       console.error(`Something went wrong. ${error.message}`);
     }
   }
 
-  // dkwefkewkf() {
-  //   const parser = new XMLParser();
-  //   const tasdasdas = readFileSync(FILE_WRITER);
-  //   const fkdfkvdf = parser.parse(tasdasdas);
-  //   return fkdfkvdf['Catalog']['Make'][0]['Model'][0];
-  // }
-
-  async getCatalogJSON() {
-    await this.getCatalogXML();
-    const sourceFile = createReadStream(FILE_WRITER);
-    const destFile = createWriteStream(TRANSFORMED_FILE_WRITER);
-    const parser = new XMLParser();
-
-    const transformToJSON = () => {
-      return new Transform({
-        transform(chunk, enc, callback) {
-          // const jsonData = parser.parse(chunk.toString());
-          // console.log(jsonData);
-          callback(null, chunk);
-        }
-      });
-    };
-
-    const dataToJSON = transformToJSON();
-    destFile.on(`finish`, () => console.log(`Файл скопирован…`));
-    sourceFile.pipe(dataToJSON).pipe(destFile);
-  }
-
   async getData() {
     const uriAccessor = this.uriAccessorFactory.getUriAccessor(this.url);
     const data = await uriAccessor.getContent();
-    return this.catalogsAdapter.convert(data);
+    return await this.catalogsAdapter.convert();
   }
+
+  // async getCatalogJSON() {
+  //   await this.getCatalogXML();
+  //   const sourceFile = createReadStream(FILE_WRITER);
+  //   const destFile = createWriteStream(TRANSFORMED_FILE_WRITER);
+  //   const transformToJSON = () => {
+  //     return new Transform({
+  //       transform(chunk, enc, callback) {
+  //         callback(null, chunk.toString());
+  //       }
+  //     });
+  //   };
+  //   const dataToJSON = transformToJSON();
+  //   destFile.on(`finish`, () => console.log(`Файл скопирован…`));
+  //   sourceFile.pipe(dataToJSON).pipe(destFile);
+  // }
 }
