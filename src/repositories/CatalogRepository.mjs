491b55d5cf4cbde7603ed1d6bc8bdc2553d2ac27
tyort <neotyo1986@gmail.com>
2022-09-06 17:35:18 +0300

    Формируем relation в CatalogRepository

--- src/repositories/CatalogRepository.mjs
+++ src/repositories/CatalogRepository.mjs
@@ -38,20 +38,74 @@ export default class CatalogRepository {
       BodyType
     } = catalogItem;
 
-    const carmanufacturer = await this.findManufactureByCode(Make[0]['_'].toLowerCase());
+    const manufacturer = await this.findManufactureByCode(Make[0]['_'].toLowerCase());
     const carbody = await this.findCarbodyByName(BodyType[0]['_'].toLowerCase());
     const cartransmission = await this.findCartransmissionByName(Transmission[0]['_'].toLowerCase());
 
+    const carmanufacturer = {
+      connectOrCreate: {
+        create: {
+          name: Make[0]['_'].toLowerCase(),
+          code: Make[0]['_'].toLowerCase(),
+          avitocode: Make[0]['_'].toLowerCase(),
+        },
+        where: {
+          code: Make[0]['_'].toLowerCase()
+        },
+      }
+    };
+
+    const carmodelbody = {
+      create: [
+        {
+          carbody: {
+            connectOrCreate: [
+              {
+                create: {
+                  name: BodyType[0]['_'].toLowerCase(),
+                  code: BodyType[0]['_'].toLowerCase(),
+                  avitocode: BodyType[0]['_'].toLowerCase()
+                },
+                where: { name: BodyType[0]['_'].toLowerCase() }
+              }
+            ]
+          }
+        }
+      ]
+    };
+
+    const carmodeltransmission = {
+      create: [
+        {
+          cartransmission: {
+            connectOrCreate: [
+              {
+                create: {
+                  name: Transmission[0]['_'].toLowerCase(),
+                  code: Transmission[0]['_'].toLowerCase(),
+                  avitocode: Transmission[0]['_'].toLowerCase()
+                },
+                where: { name: Transmission[0]['_'].toLowerCase()}
+              }
+            ]
+          }
+        }
+      ]
+    }
+
     return {
       id: Modification[0].id,
-      name: Make[0]['_'],
+      name: Model[0]['_'],
       code: null,
       enginecapacity: EngineSize[0]['_'],
       enginepower: Power[0]['_'],
       avitocode: null,
-      carmanufacturerid: carmanufacturer.id,
+      carmanufacturer,
+      carmanufacturerid: manufacturer.id,
+      carmodelbody,
       carbodyid: carbody.id,
       cartransmissionid: cartransmission.id,
+      carmodeltransmission,
       carmodelgeneration: Generation[0]['_'],
       carmodelmodification: Modification[0]['_']
     }
