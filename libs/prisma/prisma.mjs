36d1ee752dd6d5f715e4d7fd3e0829c4c13171b0
tyort <neotyo1986@gmail.com>
2022-09-07 14:30:04 +0300

    Попытка исправить ошибку "Cannot log after tests are done. Did you forget to wait for something async in your test?"

--- libs/prisma/prisma.mjs
+++ libs/prisma/prisma.mjs
@@ -1,5 +1,34 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({ log: ['query', 'info'] });
+const prisma = new PrismaClient({ log:
+  [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+  ]
+});
+
+prisma.$use(async (params, next) => {
+  const before = Date.now()
+  const result = await next(params)
+  const after = Date.now()
+  console.log(`Query ${params.model}.${params.action} took ${after - before}ms`)
+  return result
+})
+
+prisma.$on('query', (e) => {
+  console.log('Query: ' + e.query)
+  console.log('Params: ' + e.params)
+  console.log('Duration: ' + e.duration + 'ms')
+})
 
 export default prisma;
